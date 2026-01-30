2a545593f1d0a93ebb35c7fecc3f9c742d2befca
gettyuser <kumarshreeshanth4@gmail.com>
2026-01-30 11:31:58 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-    ZOOM: 3.0,
-    SMOOTHING: 0.15,
-    UNLOCK_DELAY: 250,
-    DEFAULT_IMG: 'https://images.unsplash.com/photo-1579154204601-01588f351e67?auto=format&fit=crop&q=80&w=2000'
-};
